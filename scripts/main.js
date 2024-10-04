14f32696e9894591019308c7d06774ebe3a18f73
CBorj <71759121+CBorj@users.noreply.github.com>
2024-10-04 19:02:52 +0200

    V1

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,12 @@
-document.getElementById('form-carta').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Lógica para guardar la carta
-});
+function acceso() {
+    document.getElementById('modal').style.display = 'block';
+}
 
-function brillo() {
-    // Lógica para agregar brillo al objeto h1 de clase golden-text
-    const goldenTextElements = document.querySelectorAll('.golden-text');
-    goldenTextElements.forEach(element => {
-        element.style.transition = 'text-shadow 0.5s ease-in-out, color 0.5s ease-in-out';
-        element.style.textShadow = '2px 2px 8px #FF0000';
-        element.style.color = 'yellow';
-        setTimeout(() => {
-            element.style.textShadow = '';
-            element.style.color = '';
-        }, 1000);
-    });
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+}
+
+function submitForm() {
+    // Add form submission logic here
+    closeModal();
 }
